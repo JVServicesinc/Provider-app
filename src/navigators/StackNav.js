92861d71a7132e0131b339025ec595bad3d34d15
Akhil <ezxpertapp@gmail.com>
2024-02-06 00:35:25 +0530

    iOS packages added for firebase.

--- src/navigators/StackNav.js
+++ src/navigators/StackNav.js
@@ -39,9 +39,9 @@ import ChangePassword from '../screens/auth/ChangePassword';
 import RateCard from '../screens/main/RateCard';
 import Language from '../screens/main/Language';
 import Slots from '../screens/main/Slots';
-import LanguageSplash from "../screens/splashScreen/LanguageSplash";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { MapScreen } from "../screens/main/map";
+import LanguageSplash from '../screens/splashScreen/LanguageSplash';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {MapScreen} from '../screens/main/map';
 
 const Stack = createStackNavigator();
 
@@ -50,57 +50,56 @@ export default function StackNav() {
   const [language, setLanguage] = useState(null);
 
   useEffect(() => {
-    AsyncStorage.getItem('language').then((res) => {
-      console.log(res)
+    AsyncStorage.getItem('language').then(res => {
+      console.log(res);
       if (res != null) {
         setLanguage(res);
       } else {
         setLanguage(null);
       }
     });
-
   }, []);
 
   const Screens =
     AuthReducer?.token == null
       ? {
-        Login: Login,
-        SignUp: SignUp,
-        Verification: Verification,
-        ForgotPassword: ForgotPassword,
-        OTPverification: OTPverification,
-        SignUp2: SignUp2,
-        SignUp3: SignUp3,
-        PanDetail: PanDetail,
-        SinNumber: SinNumber,
-        ChangePassword: ChangePassword,
-      }
+          Login: Login,
+          SignUp: SignUp,
+          Verification: Verification,
+          ForgotPassword: ForgotPassword,
+          OTPverification: OTPverification,
+          SignUp2: SignUp2,
+          SignUp3: SignUp3,
+          PanDetail: PanDetail,
+          SinNumber: SinNumber,
+          ChangePassword: ChangePassword,
+        }
       : {
-        TabNavigator: TabNavigator,
-        BookingDetails: BookingDetails,
-        Filter: Filter,
-        Notification: Notification,
-        Subscription: Subscription,
-        EditProfile: EditProfile,
-        BankDetails: BankDetails,
-        Refer_Earn: Refer_Earn,
-        OfflineReason: OfflineReason,
-        DelayBooking: DelayBooking,
-        WithoutMask: WithoutMask,
-        AddPicture: AddPicture,
-        WinnerAnimation: WinnerAnimation,
-        ChatScreen: ChatScreen,
-        AlertNotify: AlertNotify,
-        ServiceOtp: ServiceOtp,
-        AddServicePicture: AddServicePicture,
-        StartService: StartService,
-        MapViewPage: MapViewPage,
-        Depositmoney: Depositmoney,
-        RateCard: RateCard,
-        Language: Language,
-        Slots: Slots,
-        MapScreen: MapScreen,
-      };
+          TabNavigator: TabNavigator,
+          BookingDetails: BookingDetails,
+          Filter: Filter,
+          Notification: Notification,
+          Subscription: Subscription,
+          EditProfile: EditProfile,
+          BankDetails: BankDetails,
+          Refer_Earn: Refer_Earn,
+          OfflineReason: OfflineReason,
+          DelayBooking: DelayBooking,
+          WithoutMask: WithoutMask,
+          AddPicture: AddPicture,
+          WinnerAnimation: WinnerAnimation,
+          ChatScreen: ChatScreen,
+          AlertNotify: AlertNotify,
+          ServiceOtp: ServiceOtp,
+          AddServicePicture: AddServicePicture,
+          StartService: StartService,
+          MapViewPage: MapViewPage,
+          Depositmoney: Depositmoney,
+          RateCard: RateCard,
+          Language: Language,
+          Slots: Slots,
+          MapScreen: MapScreen,
+        };
   if (AuthReducer?.isLoading) {
     return <Splash />;
     // return <LanguageSplash />;
@@ -112,11 +111,10 @@ export default function StackNav() {
     //   // pibkc the language async - i18n change language()
     //   return <Splash />;
     // }
-
   } else {
     return (
       <NavigationContainer ref={navigationRef}>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
           {Object.entries({
             ...Screens,
           }).map(([name, component]) => {
